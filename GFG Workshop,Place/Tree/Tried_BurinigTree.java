1eb9c189d651cba6ac22459ed1e36cce0797d0d7
mohit <mohitsingh.it5@gmail.com>
2021-01-25 14:05:11 +0530

    Tried Burning Tree from Leaf node question | Mohit
    This program may fail on certain cases
    //TODO: Needed to be checked on various test cases
    
    Approach : left subtree max height + right subtree max
    height (in terms of edges)

---
+++ GFG Workshop,Place/Tree/Tried_BurinigTree.java
@@ -0,0 +1,76 @@
+// Java program to find minimum time required
+// to burn the binary tree completely
+
+public class Tried_BurinigTree {
+
+    // Tree Node
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static int height(Node node, Node cproot) {
+        if (node == null)
+            return -1;
+        int lh = height(node.left, cproot);
+        int rh = height(node.right, cproot);
+
+        if (node == cproot) {
+            res = lh + rh + 2;
+            // +1 for node(root) itself
+            // extra +1 for req ans
+        }
+        return Math.max(lh, rh) + 1;
+    }
+
+    static int ht = -1;
+    static int res;
+
+    // Driver Code
+    public static void main(String args[]) {
+
+        /*
+         * inp1: op:6 Node root = new Node(1); root.left = new Node(2); root.right = new
+         * Node(3); root.left.left = new Node(4); root.left.right = new Node(5);
+         * root.right.left = new Node(6); root.left.left.left = new Node(8);
+         * root.left.right.left = new Node(9); root.left.right.right = new Node(10);
+         * root.left.right.left.left = new Node(11);
+         */
+
+        /*
+         * inp2: op:7 1 / \ 2 3 / \ \ 4 5 6 / \ \ 7 8 9 \ 10
+         * 
+         * Node root = new Node(1);
+         * 
+         * root.left = new Node(2); root.left.left = new Node(4); root.left.right = new
+         * Node(5); root.left.right.left = new Node(7); root.left.right.right = new
+         * Node(8);
+         * 
+         * root.right = new Node(3); root.right.right = new Node(6);
+         * root.right.right.right = new Node(9); root.right.right.right.right = new
+         * Node(10);
+         */
+
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.left.right.left = new Node(7);
+        root.left.right.right = new Node(8);
+
+        root.right = new Node(3);
+        root.right.right = new Node(6);
+        root.right.right.right = new Node(9);
+        root.right.right.right.right = new Node(10);
+
+        height(root, root);
+        System.out.println(res);
+    }
+}
